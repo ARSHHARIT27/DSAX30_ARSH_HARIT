22ef612226ec826efc5e171f3d03069c5281b6c6
ARSHHARIT27 <harit27205@gmail.com>
2025-05-28 16:39:53 +0530

    change

--- Day_1/problem1.cpp
+++ Day_1/problem1.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
-#include <cmath> 
 using namespace std;
 int main() {
-    int N;
-    cout << "Enter N: ";
-    cin >> N;
-    int size = 2 * N - 1;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            int val = max(abs(N - 1 - i), abs(N - 1 - j)) + 1;
-            cout << val << " ";
+    int n;
+    cout<<"enter the value of N: ";
+    cin>>n;
+    for(int i=0;i<(2*n)-1;i++){
+        for(int j=0;j<(2*n)-1;j++){
+            int top=i;
+            int bottom=(2*n)-2-i;
+            int left=j;
+            int right=(2*n)-2-j;
+            int minm_dist;
+            if(top<bottom){
+                minm_dist=top;
+            }else{
+                minm_dist=bottom;
+            }
+            if(left<minm_dist){
+                minm_dist=left;
+            }
+            if(right<minm_dist){
+                minm_dist=right;
+            }
+            cout<<n-minm_dist<<" ";
         }
-        cout << endl;
+        cout<<endl;
     }
     return 0;
 }
